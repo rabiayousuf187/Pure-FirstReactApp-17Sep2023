3f614fd67d3af0c19c6e2b5f206121026ed0bc9b
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-17 12:18:09 +0500

    send card child as prop.children via exp. comp

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,10 @@ import Card from '../UI/Card/Card'
 const Expenses = () => {
   return (
     <>
-        <Card/>
+        <Card>
+            <p>Heading 1</p>
+            <p>Heading 2 as Card Children</p>
+        </Card>
     </>
   )
 }
--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Heading from '../../Heading/Heading';
 import './Card.css'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <>
         <div className='card'>
         <Heading title="Card Component"/>
-
+            {props.children}
         </div>
     </>
   )
