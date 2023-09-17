f1de80b6fe98e9165f5fbee06e6f895336ca25d7
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-17 12:01:02 +0500

    Start Expense Tracer App

---
+++ src/components/UI/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../../Heading/Heading';
+
+const Card = () => {
+  return (
+    <>
+        <Heading title="Card Component"/>
+    </>
+  )
+}
+
+export default Card;
