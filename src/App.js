fa2d2846db75d495f7f16a34b5b47d754b7ae58e
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-17 11:15:41 +0500

    create and used contact  js as container (Page)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@
 import Button from './components/Button/Button';
 import Heading from './components/Heading/Heading';
 import './App.css';
+import Contact from './containers/Contact';
 
 function App() {
+
   return (
     <div className="App">
        <Heading title = "Welcome to React App"/>
-        <Heading title = "React Component"/>
-        <Button title = "Click me" />
+        {/* <Heading title = "React Component"/>
+        <Button title = "Click me" /> */}
+        <Contact title="Add"/>
 
     </div>
   );
